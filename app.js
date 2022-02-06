9252a585197c2c94d8c88b5f47bffbe0d552272e
Thuong Pham <timphamvn33@gmail.com>
2022-02-05 19:56:41 -0500

    update heroku files

--- app.js
+++ app.js
@@ -187,6 +187,12 @@ app.post("/detail", (req, res)=>{
     
 })
 
-app.listen(3000, ()=>{
-    console.log("server works")
-})
+let port = process.env.PORT; 
+if(port == null || port == ""){
+ port = 3000;
+}
+
+
+app.listen(port, function() {
+  console.log("Server started on port 3000");
+});
